f7c47b70165a88c24f75b700c683f4362e4f58f5
kshelton7821 <keaton.shelton2@gmail.com>
2022-11-16 12:20:19 -0600

    In Progress

---
+++ server/client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+nohup tcpdump -i ens4 dst -w project1.pcap -c 20 &
+TCP_PID=$!
+echo "Launched TCPDUMP"
+./program $1 $2
+kill $TCP_PID
+echo "Killed TCPDUMP"
+rm nohup.out
+echo "Exit Success"
